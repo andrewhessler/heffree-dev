afdb4872386f9008cffc053107ee6686465b0acb
Andrew Hessler <adhessler@gmail.com>
2025-09-13 20:30:38 -0500

    base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use anyhow;
+use axum::{
+    http::{header, StatusCode, Uri},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use rust_embed::Embed;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    let app: Router<()> = Router::new().route("/", get(home_handler));
+
+    let addr = listener.local_addr()?;
+
+    println!("listening on {addr}");
+    _ = axum::serve(listener, app).await;
+
+    Ok(())
+}
+
+async fn home_handler() -> impl IntoResponse {
+    static_handler("/home.html".parse::<Uri>().unwrap()).await
+}
+
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/').to_string();
+
+    StaticFile(path)
+}
+
+#[derive(Embed)]
+#[folder = "src/html/"]
+struct Asset;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
+    }
 }
